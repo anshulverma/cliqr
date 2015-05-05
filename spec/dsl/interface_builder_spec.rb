bcb45f92e68e578b2bed80aa54618ca214875de2
Anshul Verma <anshul.verma86@gmail.com>
2015-05-05 01:59:24 -0700

    add test to test for non-nil config

---
+++ spec/dsl/interface_builder_spec.rb
@@ -0,0 +1,7 @@
+require 'spec_helper'
+
+describe Cliqr::CLI::Builder do
+  it 'does not allow empty config' do
+    expect { Cliqr::CLI::Builder.new(nil) }.to raise_error(Cliqr::Error::ConfigNotFound)
+  end
+end
