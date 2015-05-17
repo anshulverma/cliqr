ab3838d14da17e02745a4c6f6f1cd651d5f83589
Anshul Verma <anshul.verma86@gmail.com>
2015-05-16 20:12:36 -0700

    fix config validator spec

--- spec/config/config_validator_spec.rb
+++ spec/config/config_validator_spec.rb
@@ -4,23 +4,31 @@ require 'spec_helper'
 
 require 'cliqr/cli/config_validator'
 
+require 'fixtures/test_command'
+
 describe Cliqr::CLI::ConfigValidator do
   it 'does not allow empty config' do
-    expect { Cliqr::CLI::Interface.build(nil) }.to raise_error(Cliqr::Error::ConfigNotFound)
+    expect { Cliqr::CLI::Interface.build(nil) }.to(
+        raise_error(Cliqr::Error::ConfigNotFound, 'a valid config should be defined')
+    )
   end
 
   it 'does not allow empty basename' do
     config = Cliqr::CLI::Config.new
     config.basename = ''
     config.finalize
-    expect { Cliqr::CLI::Interface.build(config) }.to raise_error(Cliqr::Error::BasenameNotDefined)
+    expect { Cliqr::CLI::Interface.build(config) }.to(
+        raise_error(Cliqr::Error::BasenameNotDefined, 'basename not defined')
+    )
   end
 
   it 'does not allow command handler to be null' do
     config = Cliqr::CLI::Config.new
     config.basename = 'my-command'
     config.finalize
-    expect { Cliqr::CLI::Interface.build(config) }.to raise_error(Cliqr::Error::HandlerNotDefined)
+    expect { Cliqr::CLI::Interface.build(config) }.to(
+        raise_error(Cliqr::Error::HandlerNotDefined, 'handler not defined for command "my-command"')
+    )
   end
 
   it 'only accepts command handler that extend from Cliqr::CLI::Command' do
@@ -28,7 +36,10 @@ describe Cliqr::CLI::ConfigValidator do
     config.basename = 'my-command'
     config.handler = Object
     config.finalize
-    expect { Cliqr::CLI::Interface.build(config) }.to raise_error(Cliqr::Error::InvalidCommandHandler)
+    expect { Cliqr::CLI::Interface.build(config) }.to(
+        raise_error(Cliqr::Error::InvalidCommandHandler,
+                    'handler for command "my-command" should extend from [Cliqr::CLI::Command]')
+    )
   end
 
   it 'expects that config options should not be nil' do
@@ -37,6 +48,8 @@ describe Cliqr::CLI::ConfigValidator do
     config.handler = TestCommand
     config.options = nil
     config.finalize
-    expect { Cliqr::CLI::Interface.build(config) }.to raise_error(Cliqr::Error::OptionsNotDefinedException)
+    expect { Cliqr::CLI::Interface.build(config) }.to(
+        raise_error(Cliqr::Error::OptionsNotDefinedException, 'option array is nil for command "my-command"'),
+    )
   end
 end
