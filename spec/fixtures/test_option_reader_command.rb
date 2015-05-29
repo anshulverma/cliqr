dc2f473e7d7e113ae90f2d5da9d96c88196f3855
Anshul Verma <anshul.verma86@gmail.com>
2015-05-29 12:30:12 -0700

    fix TestOptionReaderCommand's description

--- spec/fixtures/test_option_reader_command.rb
+++ spec/fixtures/test_option_reader_command.rb
@@ -1,6 +1,6 @@
 # encoding: utf-8
 
-# A command that echoes the options back
+# A command that echoes the value for option named 'test-option'
 class TestOptionReaderCommand < Cliqr.command
   def execute(context)
     puts context.option('test-option').value
