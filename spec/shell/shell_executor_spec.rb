8cd1ea01352a7af46435c0341b9c5e1364cbc280
Anshul Verma <anshul.verma86@gmail.com>
2015-07-27 13:31:54 -0700

    minor: use spec_util for common functionalities

--- spec/shell/shell_executor_spec.rb
+++ spec/shell/shell_executor_spec.rb
@@ -1,8 +1,7 @@
 # encoding: utf-8
 
-require 'tempfile'
-
 require 'spec_helper'
+require 'spec_util'
 
 require 'fixtures/test_command'
 require 'fixtures/test_shell_prompt'
@@ -677,29 +676,5 @@ shell exited with code 0.
         EOS
       end
     end
-  end
-end
-
-def with_input_output(lines, &block)
-  old_stdin = $stdin
-  old_stdout = $stdout
-  input_file = Tempfile.new('cliqr').tap do |file|
-    lines.push('exit').each { |line| file.write("#{line}\n") }
-  end
-  output_file = Tempfile.new('cliqr')
-  begin
-    $stdin = input_file.open
-    $stdout = output_file.open
-    output_getter = proc  do
-      IO.read(output_file.path).gsub(/(.*)\n/, "\\1.\n")
-    end
-    block.call(output_getter)
-  ensure
-    $stdin = old_stdin
-    $stdout = old_stdout
-    input_file.close
-    output_file.close
-    input_file.unlink
-    output_file.unlink
   end
 end
